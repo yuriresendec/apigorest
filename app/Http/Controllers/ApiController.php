d4e62dd5b3bddfedca04fa5b909e1785b332749a
Yuri Resende <yuriresendec@gmail.com>
2022-03-25 17:16:44 -0300

    Completo com IDs estáticos

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,9 +10,9 @@ class ApiController extends Controller{
     public function cadastrarUsuario(){
 
         $response = Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->post('https://gorest.co.in/public/v2/users', [
-            'name' => 'Neymar Junior',
+            'name' => 'José Teste',
             'gender' => 'male',
-            'email' => 'ney@test.com',
+            'email' => 'jose@test.com',
             'status' => 'active',
         ]);
 
@@ -20,7 +20,7 @@ class ApiController extends Controller{
 
     }
 
-    public function listarUsuarios(){
+    public function getListaUsuarios(){
 
         return Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->get('https://gorest.co.in/public/v2/users');
 
@@ -39,11 +39,55 @@ class ApiController extends Controller{
         $id = 3254;
 
         $response = Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->post('https://gorest.co.in/public/v2/users/' . $id . '/posts', [
-            'title' => 'Post Neymar',
-            'body' => 'Postagem sobre Neymar',
+            'title' => 'Post teste',
+            'body' => 'Postagem de teste',
         ]);
 
         echo $response->body();
+
+    }
+
+    public function criaComment(){
+
+        $idComment = 1409;
+
+        $response = Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->post('https://gorest.co.in/public/v2/posts/' . $idComment . '/comments', [
+            'name'=> 'Mauro',
+            'email'=> 'mauro@teste.com',
+            'body'=> 'Comentário teste 3'
+        ]);
+
+        echo $response->body();
+
+    }
+
+    public function getListaPosts(){
+
+        return Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->get('https://gorest.co.in/public/v2/posts');
+
+    }
+
+    public function criaCommentPost1(){
+
+        $idComment = 1238;
+
+        $response = Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->post('https://gorest.co.in/public/v2/posts/' . $idComment . '/comments', [
+            'name'=> 'Ricardo',
+            'email'=> 'ricardo@teste.com',
+            'body'=> 'Comentário teste do Ricardo no primeiro post da lista pública'
+        ]);
+
+        echo $response->body();
+
+    }
+
+    public function apagaCommentPost1(){
+
+        $idComment = "#";
+
+        $response = Http::withtoken('a7a284bb12f3c290cdd892fb5eefe781e34e764e1d06ddd55c0d36c926fe785d')->delete('https://gorest.co.in/public/v2/comments/' . $idComment); 
+
+        echo "Comentário deletado com sucesso!";
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cadastrar-usuario', 'App\Http\Controllers\ApiController@cadastrarUsuario');
+Route::get('/cadastra-usuario', 'App\Http\Controllers\ApiController@cadastrarUsuario');
 
-Route::get('/listar-usuarios', 'App\Http\Controllers\ApiController@listarUsuarios');
+Route::get('/lista-usuarios', 'App\Http\Controllers\ApiController@getListaUsuarios');
 
 Route::get('/localiza-usuario', 'App\Http\Controllers\ApiController@localizaUsuario');
 
 Route::get('/cria-post', 'App\Http\Controllers\ApiController@criaPost');
+
+Route::get('/cria-comment', 'App\Http\Controllers\ApiController@criaComment');
+
+Route::get('/lista-posts', 'App\Http\Controllers\ApiController@getListaPosts');
+
+Route::get('/cria-comment-post-1', 'App\Http\Controllers\ApiController@criaCommentPost1');
+
